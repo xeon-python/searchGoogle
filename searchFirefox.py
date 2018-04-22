4129e6a000c18a2dc01cecbbc27c7e1799d56db3
xeon-python <33502906+xeon-python@users.noreply.github.com>
2018-04-21 23:50:19 -0500

    Update searchFirefox.py

--- searchFirefox.py
+++ searchFirefox.py
@@ -8,7 +8,6 @@ def searchGoogle(query):
     fox.get('https://www.google.com/search?q=' + query)
     foxBoxx = fox.find_element_by_class_name('g')
     foxBox = textwrap.fill(foxBoxx.text)
-    print(foxBox)
-    time.sleep(0) #@Cole Papa. If you want the browser to stay open, you can keep this line of code. Otherwise, just delete it.
     fox.quit()
+    print(foxBox)
     
